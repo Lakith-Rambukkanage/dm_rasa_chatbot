2450e406c804205783247217df8ac5fc59901fb0
Lakith-Rambukkanage <44548749+Lakith-Rambukkanage@users.noreply.github.com>
2021-11-25 06:28:50 +0530

    real agent

--- actions/search.py
+++ actions/search.py
@@ -60,7 +60,7 @@ def format_string(product):
     product_string += "පාට : " + product["colour"] + "\n"
     product_string += "අමුද්‍රව්‍ය : " + product["material"] + "\n"
     product_string += "වෙළඳනාමය : " + product["brand"] + "\n"
-    product_string += "මිල : " + product["price"] + "\n"
+    product_string += "මිල : " + str(product["price"]) + "\n"
     product_string += "හඳුනාගැනීමේ කේතය : " + product["sku"] + "\n"
 
     return product_string
